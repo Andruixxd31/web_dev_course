47e28551538e2907bb751ae4c6537a121901059e
Andruixxd31 <andruix93@gmail.com>
2020-01-19 01:35:26 -0600

    "added code to listen2.js"

--- Node/listen2.js
+++ Node/listen2.js
@@ -1,13 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    console.log('headers', request.headers); //info requested, will be showned in the terminal.
-    console.log('method', request.method);
-    console.log('url', request.url);
-    response.setHeader('Content-Type', 'text/html');
-    response.end('<h1>Hellooooooooo!</h1>');
+    //console.log('headers', request.headers); //info requested, will be showned in the terminal.
+    console.log('method', request.method); //returns http method
+    console.log('url', request.url); // url accesed
+    const user = {
+                name: 'Sally', 
+                hobby: 'Soccer'
+            }
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(user));
 })
 
 //It is listening to the connection, when it listens to it it will execute.
 //Every time the port 3000 is accesed console.log will run
-server.listen(3000) 
+server.listen(3000);
