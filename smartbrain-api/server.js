eb9ac6caff6dbadc57d180e9c53789ce960b590b
Andruixxd31 <andruix93@gmail.com>
2020-02-13 23:06:05 -0600

    added more features to the server to work well with the database

--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -26,33 +26,7 @@ const db = knex({
 // });
 
 //*-------- variables used ---------*/
-const database = {
-    users: [
-        {
-            id: '123', 
-            name: 'John',
-            email: 'J',
-            password: 'C',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124', 
-            name: 'Sally',
-            email: 'sally@example.com',
-            password: 'brownies',
-            entries: 0,
-            joined: new Date()
-        }
-    ], 
-    logging: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@example.com'
-        }
-    ]
-}
+
 
 
 //*--------components of the server ---------*/
@@ -61,30 +35,55 @@ app.get('/', (req, res) => { //*Root
 })
 
 app.post('/signin', (req, res) => { //*Signins
-    if (req.body.email === database.users[0].email 
-        && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);//express comes with a built in json response
-    } else {
-        res.status(400).json("error login in");
-    }
+    const {email, password} = req.body;
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', email)
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+                    .catch(err => res.status(400).json('unable to get user'));
+            } else {
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(err => res.status(400).json('wrong credentials'));
 })
 
 app.post('/register', (req, res) => { //*Register
     const {name, email, password} = req.body;
-    // bcrypt.hash(password, null, null, function(err, hash) {
-    //     console.log(hash);
-    // });
-    db('users')
-        .returning('*')
-        .insert({
+    const hash = bcrypt.hashSync(password);
+
+    bcrypt.compareSync("veggies", hash); // false
+
+    //use to update info in login and users always together if one fails nothing is updated
+    db.transaction(trx =>{
+        trx.insert({ 
+            hash: hash,
             email: email,
-            name: name,
-            joined: new Date() 
         })
-        .then(response => {
-            res.json(response);
+        .into('login') //update login
+        .returning('email')
+        .then(loginEmail => { //use the login email to retunr another trx transaction
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0],
+                name: name,
+                joined: new Date() 
+            })
+            .then(response => {
+                res.json(response);
+            })
         })
-        .catch(err => res.status(400).json('unable to register'))
+    .then(trx.commit) //commits if everything works
+    .catch(trx.rollback) //doesn't do the transaction if anything fails.
+    }) 
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
@@ -101,19 +100,15 @@ app.get('/profile/:id', (req, res) => {
         res.status(400).json(err.message))
 })
 
-app.put('/image', (req, res) => { //*Root
+app.put('/image', (req, res) => { 
     const {id} = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        } 
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
     })
-    if(!found){
-        res.status(404).json('user not found');
-    }
+    .catch(err => res.status(400).json("unable to get count"));
 })
 
 
